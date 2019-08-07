99aadb2851294616199d30c6c678423346caeb0e
Diego1906 <di_fsantos@yahoo.com.br>
2019-08-07 01:20:06 -0300

    Implementação Snackbar

--- app/src/main/java/com/teste/componentes/MainActivity.kt
+++ app/src/main/java/com/teste/componentes/MainActivity.kt
@@ -3,6 +3,8 @@ package com.teste.componentes
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -30,15 +32,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 toast.view = toastLayout
 
                 val textViewLayout = toast.view.findViewById<TextView>(R.id.textMessage)
-                textViewLayout.setTextColor(Color.RED)
+                textViewLayout.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
                 textViewLayout.text = "Toast Notifications!"
 
                 toast.show()
+            }
+            R.id.buttonSnackMe -> {
+                val snackbar = Snackbar.make(constraintLayout, "Snackbar Notification!", Snackbar.LENGTH_LONG)
+
+                // Cor do texto
+                snackbar.view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+                    .setTextColor(ContextCompat.getColor(this,  R.color.colorOrange))
+
+                // Cor do background
+                //snackbar.view.setBackgroundColor(Color.BLUE)
+
+                // Para pegar a cor do arquivo de colors usar ( ContextCompat.getColor(this, R.color.colorBlue) )
+                snackbar.view.setBackgroundColor(ContextCompat.getColor(this, R.color.colorBlue))
+
+                // Mostrar imp da action
+                snackbar.setAction("Desfazer", {
+                    Snackbar.make(constraintLayout, "Ação desfeita", Snackbar.LENGTH_SHORT).show()
+                })
+
+                // Mudar cor do texto da action
+                snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.colorPurple))
+
+                snackbar.show()
             }
         }
     }
 
     private fun setListeners() {
         buttonToastMe.setOnClickListener(this)
+        buttonSnackMe.setOnClickListener(this)
     }
 }
